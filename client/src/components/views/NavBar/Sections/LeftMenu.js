bce290144e4d07553da6c89b30a34b46fd762649
Yousha Mahamuni <40205524+yousha806@users.noreply.github.com>
2021-01-01 12:44:51 +0530

    Add files via upload

---
+++ client/src/components/views/NavBar/Sections/LeftMenu.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Menu } from 'antd';
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
+
+function LeftMenu(props) {
+  return (
+    <Menu mode={props.mode}>
+    <Menu.Item key="mail">
+      <a href="/"><b>Home</b></a>
+    </Menu.Item>
+  </Menu>
+  )
+}
+
+export default LeftMenu
---
+++ server/config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI:'mongodb+srv://Shrikar_2:shrikar2@cluster1.kpdvi.mongodb.net/Test?retryWrites=true&w=majority'
+};
